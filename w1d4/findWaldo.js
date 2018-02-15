59d0570dfaafb859b46f6b52776d78503fa42f1d
Philip <philiplouietemplonuevo@yahoo.ca>
2018-02-15 19:16:57 +0000

    Refactored findWaldo using forEach

--- w1d4/findWaldo.js
+++ w1d4/findWaldo.js
@@ -1,14 +1,24 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);// execute callback
-    }
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);// execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(arr) {
+//     console.log("Found Waldo at index " + arr + "!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+function actionWhenFound(element,index,array) {
+  if (array[index] === "Waldo") {
+    console.log("Found Waldo at index " + index + '!');
   }
 }
 
-function actionWhenFound(arr) {
-    console.log("Found Waldo at index " + arr + "!");
-}
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+["Alice", "Bob", "Waldo", "Winston"].forEach(actionWhenFound);
